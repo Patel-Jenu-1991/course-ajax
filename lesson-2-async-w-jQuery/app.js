6339338cf588b8cfa561401be6517c7afbfff0d9
Jaineshkumar Patel <jenu.16@icloud.com>
2018-09-02 18:55:22 -0500

    use ajax with jQuery to request data asynchronously

--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -10,5 +10,52 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID <Your-API-Key-Here>'
+            }
+        }).done(addImage).fail(function (err) {
+            requestError(err, 'image');
+        });
+
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<Your-API-Key-Here>`
+        }).done(addArticles).fail(function (err) {
+            requestError(err, 'articles');
+        });
     });
+
+    function addImage(data) {
+        let htmlContent = '';
+        if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+              </figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No Images Available</div>';
+        }
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles(data) {
+        let htmlContent = '';
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+              <h2><a href="${article.web_url}">${article.headline.main}</h2>
+              <p>${article.snippet}</p>
+              </li>`).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning error-${part}">Oh no! ${part} did not load!</p>`);
+    }
 })();
